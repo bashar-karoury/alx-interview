fa117e62824577cc6e3313e6ea6fbe8aab4f06f2
bashar_karoury <basharalkaroury@hotmail.com>
2024-11-20 22:07:20 +0300

    fix sorting

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -13,33 +13,45 @@ if (!movieId) {
   process.exit(-1);
 }
 
+// create new Promise to wrap request
+const fetchCharacter = function (url) {
+  return new Promise((resolve, reject) => {
+    request(url, (err, response, body) => {
+      if (err) {
+        reject(err);
+      } else if (response.statusCode === 200) {
+        const data = JSON.parse(body);
+        // console.log(data.name); // Print character name
+        resolve(data.name);
+      } else {
+        reject(new Error(`Request failed with status code: ${response.statusCode}`));
+      }
+    });
+  });
+};
+
 const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 // Making the GET request
-request(url, (error, response, body) => {
-  if (error) {
-    console.error('An error occurred:', error);
-    return;
-  }
-
-  // Checking the status code
-  if (response.statusCode === 200) {
-    const data = JSON.parse(body);
-    const characters = data.characters;
-    // for each character print its name
-    for (const character of characters) {
-      request(character, (err, response, body) => {
-        if (error) {
-          console.error(err);
-        }
-        if (response.statusCode === 200) {
-          const data = JSON.parse(body);
-          console.log(data.name);
-        } else {
-          console.log(`Request failed with status code: ${response.statusCode}`);
-        }
-      });
+const getCharacters = async function () {
+  request(url, async (error, response, body) => {
+    if (error) {
+      console.error('An error occurred:', error);
+      return;
     }
-  } else {
-    console.log(`Request failed with status code: ${response.statusCode}`);
-  }
-});
+
+    // Checking the status code
+    if (response.statusCode === 200) {
+      const data = JSON.parse(body);
+      // console.log(data);
+      const characters = data.characters;
+      // for each character print its name
+      for (const character of characters) {
+        const characterName = await fetchCharacter(character);
+        console.log(characterName);
+      }
+    } else {
+      console.log(`Request failed with status code: ${response.statusCode}`);
+    }
+  });
+};
+getCharacters();

197290ca3fdb8104d4adff0afcf7722b732bcf01
bashar_karoury <basharalkaroury@hotmail.com>
2024-12-12 18:08:13 +0300

    Updated files: 0-prime_game.py

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -11,6 +11,8 @@ def isWinner(x, nums):
         Return: name of the player that won the most rounds
             If the winner cannot be determined, return None
     """
+    if x < 1:
+        return None
     if x > len(nums):
         return None
     if not nums:

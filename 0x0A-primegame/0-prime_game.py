edcc1cafe002e12e8707701fe155f98158896ba2
bashar_karoury <basharalkaroury@hotmail.com>
2024-12-12 19:35:17 +0300

    Updated files: 0-prime_game.py

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -21,30 +21,25 @@ def isWinner(x, nums):
         return None
     score = [0, 0]
     i = 0
-    while (i < x):
-        # all initially primes
-        # print(f'Selecting num {nums[i]}')
-        lst = [True for i in range(0, nums[i] + 1)]
-        lst[0] = False
-        lst[1] = False
-        # alternate turns till all lst is False
-        turn = 1
-        while True in lst:
-            turn = not turn
-            turner = 'Maria' if not turn else 'Ben'
-            # print(f'turn of {turner}')
-            # get first True idx = 2
-            idx = lst.index(True)
-            # print(f'first idx {idx}')
-            lst[idx] = False
-            # Falsify all multiples
-            for idx in range(idx * 2, len(lst), idx):
+
+    max_n = max(nums)
+
+    lst = [True for i in range(0, max_n + 1)]
+    lst[0] = False
+    lst[1] = False
+    for i in range(len(lst)):
+        # print('i ',i)
+        if lst[i]:
+            for idx in range(i * 2, len(lst), i):
                 lst[idx] = False
-            # print(lst)
-        score[turn] += 1
-        turner = 'Maria' if not turn else 'Ben'
-        # print(f'Winner is {turner}')
-        i += 1
+    # print(lst)
+    for _ in range(x):
+        # get primes till nums[_]
+        primes = list(filter(lambda y: lst[y], range(2, nums[_] + 1)))
+        if len(primes) % 2 == 0:
+            score[1] += 1
+        else:
+            score[0] += 1
     # print('score', score)
     if score[0] > score[1]:
         return "Maria"
